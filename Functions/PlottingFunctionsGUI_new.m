fdb789780c542b6f1a0953990d7665db4fe6b8b2
ltao31 <liangyu.tao1001@gmail.com>
2021-02-24 22:02:08 -0500

    ENH: PSTH added

--- Functions/PlottingFunctionsGUI_new.m
+++ Functions/PlottingFunctionsGUI_new.m
@@ -3,32 +3,57 @@ function [] = PlottingFunctionsGUI_new(app,peakAll,SpikesAllV,Stimulus,IdxInClus
 c = distinguishable_colors(app.nClust);
 
 nCluster = numel(IdxInCluster);
+npt = numel(app.LFP);
 
 %% main plot
 % Plotting voltage trace with the different types of spikes overlayed in 
 % different color *.
 tt = (1:numel(app.voltageBS))./app.fs;
 cla(app.axMain,'reset')
+yyaxis(app.axMain,'left');hold(app.axMain,'on');
 plot(app.axMain,tt,app.voltageBS./app.Scale_fact);
 hold(app.axMain,'on');
 for i = 1:nCluster
     idxK = IdxInCluster{i};
     plot(app.axMain,tt(peakAll(idxK)),app.voltageBS(peakAll(idxK))./app.Scale_fact,'*','Color',c(i,:))
 end
-if ~isempty(Stimulus)
-    plot(app.axMain,tt,Stimulus,'k','Linewidth',2)
-end
 xlabel(app.axMain,'Time (s)');ylabel(app.axMain,'mV')
+if ~isempty(Stimulus)
+    yyaxis(app.axMain,'right');
+    plot(app.axMain,tt,Stimulus,'k','Linewidth',2)
+    ylabel(app.axMain,'Stim (V)')
+    ylim(app.axMain,[min(app.voltageBS./app.Scale_fact) 5])
+end
 title(app.axMain,app.fileName, 'interpreter', 'none')
 hold(app.axMain,'off');
+
+app.axMain.YAxis(1).Color = 'b';
+app.axMain.YAxis(2).Color = 'k';
 
 %% LFP plot
 % Plotting voltage trace with the different types of spikes overlayed in 
 % different color *.
-tt = (1:numel(app.LFP))./app.fs;
+tt = (1:npt)./app.fs;
 cla(app.axLFP,'reset')
-plot(app.axLFP,tt,app.LFP);
-xlabel(app.axLFP,'Time (s)');ylabel(app.axLFP,'mV')
+yyaxis(app.axLFP,'left');hold(app.axLFP,'on');
+for k = 1:nCluster
+    idxK = IdxInCluster{k};
+    spkTime = peakAll(idxK);
+    
+    [optN, ~, ~] = sshist(spkTime,1:50);
+    optN = ceil(optN./(max(spkTime)-min(spkTime)).*npt);
+    edges = linspace(0,npt,optN);  % Optimal segmentation
+    curr_psth = histcounts(spkTime,edges)./(edges(2)-edges(1)).*app.fs;
+    stairs(app.axLFP,(edges(1:end-1)+edges(2:end))./2./app.fs,curr_psth,'-','Color',c(k,:));
+end
+xlabel(app.axLFP,'Time (s)');ylabel(app.axLFP,'PSTH (Hz)')
+yyaxis(app.axLFP,'left');hold(app.axLFP,'off');
+
+yyaxis(app.axLFP,'right');plot(app.axLFP,tt,app.LFP,'k-');
+xlabel(app.axLFP,'Time (s)');ylabel(app.axLFP,'LFP (mV)')
+
+app.axLFP.YAxis(1).Color = 'k';
+app.axLFP.YAxis(2).Color = 'k';
 
 %% Plotting spikes overlayed on top of one another by cluster (waveform)
 % tempFHandle = {fHandle.axS1,fHandle.axS2,fHandle.axS3,fHandle.axS4,fHandle.axS5,fHandle.axS6};
